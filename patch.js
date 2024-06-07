b492ba508819a78e645d7a50a52f1cb99a170bf1
UiharuKazari2008 <nomail@internet.net>
2024-06-07 03:08:04 -0400

    Update

--- patch.js
+++ patch.js
@@ -1,8 +1,20 @@
 (async () => {
-    const patches = require("./patches.json")
     const fs = require('fs');
     const path = require('path');
     const request = require('request');
+
+    let patches = {};
+
+    try {
+        if (fs.existsSync(path.resolve(path.join('Q:\\lib\\rtp\\', 'patches.json')))) {
+            patches = JSON.parse(fs.readFileSync(path.resolve(path.join('Q:\\lib\\rtp\\', 'patches.json'))).toString());
+        } else {
+            console.log("Patch matrix does not exist!");
+            process.exit(0);
+        }
+    } catch (e) {
+        console.error("Failed to load patch matrix", e.message)
+    }
 
     function patchHex(input, patchTable, patchState) {
         try {
@@ -10,7 +22,7 @@
             let fileBuffer = input;
             const patchBytes = patchState ? Buffer.from(patchTable.on) : Buffer.from(patchTable.off);
             patchBytes.copy(fileBuffer, offset);
-            console.log(`Patch ${patchTable.offset} = ${patchState} applied successfully.`);
+            //console.log(`Patch ${patchTable.offset} = ${patchState} applied successfully.`);
             return fileBuffer;
         } catch (error) {
             console.error('Failed to apply patch:', error.message);
@@ -19,7 +31,7 @@
     }
 
     const remote_options = await new Promise(ok => {
-        request(`http://127.0.0.1:6799/lce/rtopts/config.json`, {
+        request(`http://127.0.0.1:6799/lce/rtopts/config.json?raw=true`, {
             timeout: 15000
         }, async (error, response, body) => {
             if (!error && response.statusCode === 200) {
@@ -38,7 +50,25 @@
         })
     })
     if (remote_options) {
-        const enabledPatches = Object.keys(remote_options);
+        let base_patches = [];
+        let base_patches_toggles = {};
+        let versioned_patches = {};
+        let versioned_patches_toggles = {};
+        Object.keys(remote_options)
+            .filter(k => remote_options[k] !== undefined || remote_options[k] !== null)
+            .map(k => {
+                const j = k.toString().toLowerCase().split(':');
+                base_patches.push(j[0]);
+                base_patches_toggles[j[0]] = remote_options[k];
+                if (j.length > 1) {
+                    const kj = j[1];
+                    if (!versioned_patches[kj])
+                        versioned_patches[kj] = [];
+                    versioned_patches[kj].push(j[0]);
+                    versioned_patches_toggles[j[0]] = remote_options[k];
+                }
+            });
+        const versions = Object.keys(versioned_patches);
 
         await Promise.all(Object.keys(patches).map(k => {
             return {key: k, ...patches[k]}
@@ -47,9 +77,16 @@
                 const filePath = path.resolve((e.path.includes(':\\')) ? e.path : path.join("X:\\", e.path))
                 if (fs.existsSync(filePath)) {
                     let fileBuffer = await fs.readFileSync(filePath);
-                    Object.keys(e.options).map(f => {
+                    Object.keys(e.options).filter(f => base_patches.includes(f.toLowerCase())).map(f => {
                         e.options[f].patches.map(g => {
-                            fileBuffer = patchHex(fileBuffer, g, (enabledPatches.includes(f.toLowerCase())))
+                            fileBuffer = patchHex(fileBuffer, g, (base_patches_toggles[f.toLowerCase()]))
+                        })
+                    })
+                    Object.keys(e.versions).filter(f => versions.includes(f.toLowerCase())).map(f => {
+                        Object.keys(e.versions[f]).map(g => {
+                            e.versions[f][g].patches.map(h => {
+                                fileBuffer = patchHex(fileBuffer, h, (versioned_patches_toggles[g.toLowerCase()]))
+                            })
                         })
                     })
                     await fs.writeFileSync(filePath, fileBuffer);
