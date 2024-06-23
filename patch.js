ffd57700312505f0ccb8ffe2a625bb4ea3848f7a
UiharuKazari2008 <nomail@internet.net>
2024-06-23 18:25:37 -0400

    Update

--- patch.js
+++ patch.js
@@ -6,7 +6,9 @@
     let patches = {};
 
     try {
-        if (fs.existsSync(path.resolve(path.join('Q:\\lib\\rtp\\', 'patches.json')))) {
+        if (fs.existsSync(path.resolve(path.join('X:\\', 'patches.json')))) {
+            patches = JSON.parse(fs.readFileSync(path.resolve(path.join('X:\\', 'patches.json'))).toString());
+        } else if (fs.existsSync(path.resolve(path.join('Q:\\lib\\rtp\\', 'patches.json')))) {
             patches = JSON.parse(fs.readFileSync(path.resolve(path.join('Q:\\lib\\rtp\\', 'patches.json'))).toString());
         } else {
             console.log("Patch matrix does not exist!");
@@ -30,42 +32,51 @@
         }
     }
 
-    const remote_options = await new Promise(ok => {
-        request(`http://127.0.0.1:6799/lce/rtopts/config.json?raw=true`, {
-            timeout: 15000
-        }, async (error, response, body) => {
-            if (!error && response.statusCode === 200) {
-                try {
-                    const json = JSON.parse(body.toString());
-                    ok(json);
-                } catch (e) {
-                    ok(false);
-                }
-            } else {
-                console.error("Failed to get valid configuration from LCC");
-                if (error)
-                    console.error(error.message)
-                ok(undefined);
+    const enabled_options = await (async () => {
+        if (fs.existsSync(path.resolve(path.join('Y:\\', 'enabled_options.json')))) {
+            try {
+                return JSON.parse(fs.readFileSync(path.resolve(path.join('Y:\\', 'enabled_options.json'))).toString());
+            } catch (e) {
+                console.error("Failed to load enabled options", e.message)
             }
+        }
+        return new Promise(ok => {
+            request(`http://127.0.0.1:6799/lce/rtopts/config.json?raw=true`, {
+                timeout: 15000
+            }, async (error, response, body) => {
+                if (!error && response.statusCode === 200) {
+                    try {
+                        const json = JSON.parse(body.toString());
+                        ok(json);
+                    } catch (e) {
+                        ok(false);
+                    }
+                } else {
+                    console.error("Failed to get valid configuration from LCC");
+                    if (error)
+                        console.error(error.message)
+                    ok(undefined);
+                }
+            })
         })
-    })
-    if (remote_options) {
+    })()
+    if (enabled_options) {
         let base_patches = [];
         let base_patches_toggles = {};
         let versioned_patches = {};
         let versioned_patches_toggles = {};
-        Object.keys(remote_options)
-            .filter(k => remote_options[k] !== undefined || remote_options[k] !== null)
+        Object.keys(enabled_options)
+            .filter(k => enabled_options[k] !== undefined || enabled_options[k] !== null)
             .map(k => {
                 const j = k.toString().toLowerCase().split(':');
                 base_patches.push(j[0]);
-                base_patches_toggles[j[0]] = remote_options[k];
+                base_patches_toggles[j[0]] = enabled_options[k];
                 if (j.length > 1) {
                     const kj = j[1];
                     if (!versioned_patches[kj])
                         versioned_patches[kj] = [];
                     versioned_patches[kj].push(j[0]);
-                    versioned_patches_toggles[j[0]] = remote_options[k];
+                    versioned_patches_toggles[j[0]] = enabled_options[k];
                 }
             });
         const versions = Object.keys(versioned_patches);
